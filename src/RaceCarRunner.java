46bb8c47ec713979f21ec529d0534f4642e7aeac
camman00 <league@WTS-IM-17.attlocal.net>
2019-03-24 17:25:24 -0700

    i finished cat and race car

--- src/RaceCarRunner.java
+++ src/RaceCarRunner.java
@@ -5,14 +5,18 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+ RaceCar g 	= new RaceCar("Hennessey Venom", 5);
 		// 2. Print the RaceCar's position in the race
-
+int h = g.getPositionInRace();
+System.out.println(h);
 		// 3. Crash the RaceCar
-		
+		g.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+if(g.isDamaged()) {
+g.pit();}
 		// 5. Help the car move into first place.
-
+for (int i = 0; i < 7; i++) {
+	
+g.overtake();
 	}
-}
+}}
---
+++ src/catRunnner.java
@@ -0,0 +1,12 @@
+
+public class catRunnner {
+ public static void main(String[] args) {
+	Cat meow = new Cat("mr.meow");
+meow.meow();
+meow.printName();
+for (int i = 0; i < 9; i++) {
+	
+
+meow.kill();
+}
+}}
